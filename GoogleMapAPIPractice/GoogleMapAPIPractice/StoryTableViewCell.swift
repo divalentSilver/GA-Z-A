07d819980c0a6a19e90b94ddfc7aa8848ca706b7
Sinyoung Jun <alongthestar@naver.com>
2018-01-31 15:13:28 +0900

    StoryTableView화면구성
    데이터셋에서 정보 불러와야 함

---
+++ GoogleMapAPIPractice/GoogleMapAPIPractice/StoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoryImageTableViewCell.swift
+//  GoogleMapAPIPractice
+//
+//  Created by cscoi014 on 2018. 1. 31..
+//  Copyright © 2018년 KaEun Rhee. All rights reserved.
+//
+
+import UIKit
+
+class StoryImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GoogleMapAPIPractice/GoogleMapAPIPractice/StoryTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  StoryTableViewController.swift
+//  GoogleMapAPIPractice
+//
+//  Created by cscoi014 on 2018. 1. 31..
+//  Copyright © 2018년 KaEun Rhee. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewController: UITableViewController {
+
+    @IBOutlet weak var StoryImageView: UIImageView!
+    @IBOutlet weak var StoryDateLabel: UILabel!
+    @IBOutlet weak var StoryPlaceLabel: UILabel!
+    @IBOutlet weak var StoryMemoLabel: UILabel!
+/////StoryMemoLabel이 Edit하면 사용자가 작성한 내용으로 변경되어야 함
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return .count///////////////이미지 개수 카운트 해서 row 생성
+    }
+
+/////////////스토리 내 각 이미지+메모 구성
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryTableViewCell", for: indexPath)
+
+        cell.StoryImageView?.///////////데이터셋 각 이미지
+        cell.StoryDateLabel?.
+        cell.StoryPlaceLabel?.
+        cell.StoryMemoLabel?.
+        
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
